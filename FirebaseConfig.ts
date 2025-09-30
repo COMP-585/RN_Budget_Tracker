4053b6881be3d1f6426633ea358ed6de4ff6884c
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2025-09-29 17:45:31 -0700

    Separated logged in pages

---
+++ FirebaseConfig.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { initializeAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdkdxFV_GBzn_QbdbmDFXUbMYCCwVEzg0",
+  authDomain: "comp-585-savings-goal-app.firebaseapp.com",
+  projectId: "comp-585-savings-goal-app",
+  storageBucket: "comp-585-savings-goal-app.firebasestorage.app",
+  messagingSenderId: "75063085321",
+  appId: "1:75063085321:web:f47ed68163452ccff26bd8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app);
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import "@/global.css";
 import { NAV_THEME } from "@/lib/theme";
 import { ThemeProvider } from "@react-navigation/native";
 import { PortalHost } from "@rn-primitives/portal";
@@ -5,15 +6,27 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "nativewind";
 import React from "react";
-import "../global.css";
+
+const isLoggedIn = false; // Replace with your auth logic
+const shouldCreateAccount = false; // Replace with your logic
 
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
   return (
     <ThemeProvider value={NAV_THEME[colorScheme ?? "light"]}>
       <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
-      <Stack />
-      <PortalHost />;
+      <Stack>
+        <Stack.Protected guard={isLoggedIn}>
+          <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+        </Stack.Protected>
+        <Stack.Protected guard={!isLoggedIn}>
+          <Stack.Screen name="sign-in" />
+          <Stack.Protected guard={shouldCreateAccount}>
+            <Stack.Screen name="create-account" />
+          </Stack.Protected>
+        </Stack.Protected>
+      </Stack>
+      <PortalHost />
     </ThemeProvider>
   );
 }
